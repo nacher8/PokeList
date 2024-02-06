e13a4b1148a9b7e00c1d536a977a48078650ff51
Ignacio Hernaiz <nacho_celtic@hotmail.com>
2024-02-06 10:13:50 +0100

    Initial Commit

---
+++ PokeList/PokeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeListApp.swift
+//  PokeList
+//
+//  Created by IGNACIO HERNAIZ IZQUIERDO on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
